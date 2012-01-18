3794cbbccd2355d20192d35c57a80523d74815dc
sylvain.le-gall <sylvain.le-gall@polytechnique.org>
2012-01-18 22:14:41 +0100

    Update examples
    Ignore-this: add228531f931293ffe6ac37baf7ddac
    
    darcs-hash:20120118211441-4d692-afb1ef9a1173279c88822bc2e6572a374fa253d9

--- examples/example.ml
+++ examples/example.ml
@@ -15,12 +15,4 @@ let test_list_append _ =
 let suite = "OUnit Example" >::: ["test_list_length" >:: test_list_length;
 				  "test_list_append" >:: test_list_append]
 let _ =
-  let verbose = ref false in
-  let set_verbose _ = verbose := true in
-  Arg.parse
-    [("-verbose", Arg.Unit set_verbose, "Run the test in verbose mode.");]
-    (fun x -> raise (Arg.Bad ("Bad argument : " ^ x)))
-    ("usage: " ^ Sys.argv.(0) ^ " [-verbose]");
-  if not (was_successful (run_test_tt ~verbose:!verbose suite)) then
-    exit 1
-
+  run_test_tt_main suite
