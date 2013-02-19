753a1348c5201e3373c113b7304947868836a503
sylvain.le-gall <sylvain.le-gall@polytechnique.org>
2013-02-20 00:41:43 +0100

    Fix ardivink setup.
    Ignore-this: a305f9cd0b332125570ef9a945d469c2
    
    darcs-hash:20130219234143-4d692-db8b0b0fc2406150383ad0eb9d1d2f312d27da77

--- ardivink.lua
+++ ardivink.lua
@@ -9,7 +9,7 @@ darcs.init()
 
 ci.prependenv("PATH", "/usr/opt/godi/bin")
 ci.prependenv("PATH", "/usr/opt/godi/sbin")
-ci.putenv("OUNIT_HTML_OUTPUT_DIR", dist.make_filename("ounit-log.html"))
+ci.putenv("OUNIT_OUTPUT_HTML_DIR", dist.make_filename("ounit-log.html"))
 ci.putenv("OUNIT_OUTPUT_FILE", dist.make_filename("ounit-log.txt"))
 
 oasis.std_process()
