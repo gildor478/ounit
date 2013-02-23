edeaa5b75706a31c57aa13e8a475255024c4808d
sylvain.le-gall <sylvain.le-gall@polytechnique.org>
2013-02-23 18:31:39 +0100

    Enable JUnit out for ardivink.
    Ignore-this: 7a014ec9ebb8b4991d83f51f3299e06a
    
    darcs-hash:20130223173139-4d692-e2ee21107cd79f5d40fc96f679e55d83ef05175d

--- ardivink.lua
+++ ardivink.lua
@@ -10,6 +10,7 @@ darcs.init()
 ci.prependenv("PATH", "/usr/opt/godi/bin")
 ci.prependenv("PATH", "/usr/opt/godi/sbin")
 ci.putenv("OUNIT_OUTPUT_HTML_DIR", dist.make_filename("ounit-log.html"))
+ci.putenv("OUNIT_OUTPUT_JUNIT_FILE", dist.make_filename("junit.xml"))
 ci.putenv("OUNIT_OUTPUT_FILE", dist.make_filename("ounit-log.txt"))
 
 oasis.std_process()
