3104496a18b235a960a3a3dc0316c8077715866c
sylvain.le-gall <sylvain.le-gall@polytechnique.org>
2013-08-28 01:30:17 +0200

    Implement tests_running field in state.
    Ignore-this: 661cd5d63973e0d95bd183016c0d4ca5
    
    darcs-hash:20130827233017-4d692-48a0724395529fe72f924d75ceac925eccad45da

--- src/oUnitState.ml
+++ src/oUnitState.ml
@@ -7,6 +7,7 @@ open OUnitTypes
 type t =
     {
       tests_planned: (path * test_fun) list;
+      tests_running: (path * test_fun) list;
       results: test_results;
       chooser: t -> (path * test_fun);
     }
@@ -15,6 +16,7 @@ let create chooser test_cases =
   {
     results = [];
     tests_planned = test_cases;
+    tests_running = [];
     chooser = chooser;
   }
 
@@ -25,8 +27,8 @@ let add_test_result test_result state =
     {
       results = test_result :: state.results;
       tests_planned = filter_out test_path state.tests_planned;
+      tests_running = filter_out test_path state.tests_running;
       chooser = state.chooser;
-      (* TODO: add tests_running. *)
     }
 
 let next_test_case state =
@@ -37,7 +39,7 @@ let next_test_case state =
         let (test_path, _) as test_case = state.chooser state in
           Some test_case,
           {state with
-              (* TODO: add tests_running. *)
+               tests_running = test_case :: state.tests_running;
                tests_planned = filter_out test_path state.tests_planned}
 
 let get_results state = state.results
